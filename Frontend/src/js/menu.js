7dc81cccf2444af5504c1bde42988acfdd079347
Irene Medina García <imedgar@epsg.upv.es>
2024-10-22 17:23:36 +0200

    Cambios Irene |
    Menú desplegable que ya no aparece en ventanas grandes aunque se deje activado

--- Frontend/src/js/menu.js
+++ Frontend/src/js/menu.js
@@ -1,7 +1,19 @@
 const mobileMenu = document.getElementById("mobile-menu");
 const navLinks = document.querySelector(".nav-links");
 
+// Evento para abrir/cerrar el menú en móviles
 mobileMenu.addEventListener("click", () => {
   navLinks.classList.toggle("show");
   mobileMenu.classList.toggle("active");
 });
+
+// Función para cerrar el menú cuando se cambia a pantallas grandes
+function handleResize() {
+  if (window.innerWidth > 768) {
+    navLinks.classList.remove("show");  // Oculta el menú
+    mobileMenu.classList.remove("active");  // Asegura que el botón vuelva a su estado inicial
+  }
+}
+
+// Escucha cambios en el tamaño de la ventana
+window.addEventListener("resize", handleResize);
