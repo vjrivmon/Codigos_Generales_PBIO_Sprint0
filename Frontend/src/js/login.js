16b05e075d4594c2fff61914afccad15be76f6bf
PabloMeana <pmeagon@epsg.upv.es>
2024-10-25 17:15:44 +0200

    Cambios Pablo |
    Registrar usuario funciona

--- Frontend/src/js/login.js
+++ Frontend/src/js/login.js
@@ -41,6 +41,31 @@ async function verificarUsuario(email, password) {
         alert('Ocurrió un error al verificar el usuario. Inténtalo de nuevo más tarde.');
     }
 }
+// Función para registrar un nuevo usuario
+async function registrarUsuario(name, email, password) {
+    try {
+        const response = await fetch('http://192.168.0.101:8080/usuarios', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ nombre: name, correo: email, contrasena: password }) // Asegúrate de que estos nombres coincidan con los de tu base de datos
+        });
+
+        if (!response.ok) {
+            throw new Error('Error en la respuesta de la API: ' + response.status);
+        }
+
+        const data = await response.json();
+        alert('Usuario registrado exitosamente!'); // Mensaje de éxito
+
+        // Opcionalmente, puedes redirigir al usuario al iniciar sesión
+        container.classList.remove("active"); // Volver a la vista de inicio de sesión
+    } catch (error) {
+        console.error('Error al registrar el usuario:', error);
+        alert('Ocurrió un error al registrar el usuario. Inténtalo de nuevo más tarde.');
+    }
+}
 
 // Manejar el evento de clic en el botón de iniciar sesión
 document.getElementById('botonIniciarSesion').addEventListener('click', function() {
@@ -48,4 +73,14 @@ document.getElementById('botonIniciarSesion').addEventListener('click', function
     const password = document.getElementById('loginPassword').value;
 
     verificarUsuario(email, password); // Llamar a la función para verificar el usuario
+});
+
+// Manejar el evento de clic en el botón de registrarse
+document.getElementById('botonRegistrase').addEventListener('click', function(event) {
+    event.preventDefault(); // Evitar el envío del formulario
+
+    const email = document.getElementById('signUpEmail').value; // Obtener el correo
+    const password = document.getElementById('signUpPassword').value; // Obtener la contraseña
+
+    registrarUsuario(name, email, password); // Llamar a la función para registrar el usuario
 });
