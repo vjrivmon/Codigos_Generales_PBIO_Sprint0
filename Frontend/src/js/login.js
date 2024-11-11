9b678bec436319f057706e285d07ad640975c7a3
Vicente Rivas Monferrer <vjrivmon@epsg.upv.es>
2024-11-11 16:59:12 +0100

    Cambios Vicente |
    Solucionada cagada de Pablo

--- Frontend/src/js/login.js
+++ Frontend/src/js/login.js
@@ -3,31 +3,26 @@ const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
 
 registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
+    container.classList.add("active");
 });
 
 loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
+    container.classList.remove("active");
 });
 
 document.getElementById("privacy-policy").addEventListener("change", function() {
-            document.getElementById("register-btn").disabled = !this.checked;
-        });
+            document.getElementById("register-btn").disabled = !this.checked;
+        });
 
 // Función para registrar un nuevo usuario
-async function registrarUsuario( email, password) {
+async function registrarUsuario(email, password, phone, name) {
     try {
-        const response = await fetch('http://192.168.0.101:8080/usuarios', { // Ip Torre Pablo
-        //const response = await fetch('http://172.20.10.11:8080/usuarios', { // Ip Portatil Pablo Wifi Pablo
-        //const response = await fetch('http://192.168.0.20:8080/usuarios', { // Ip Ordenador Vicente
-        //const response = await fetch('http://192.168.0.17:8080/usuarios', { // Ip Ordenador Irene
-        //const response = await fetch('http://172.20.10.5:8080/usuarios', { // Ip Portátil visi Universidad
-        
+        const response = await fetch('http://192.168.0.25:8080/usuarios', { // Ip Portátil visi Universidad
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ correo: email, contrasena: password }) // Asegúrate de que estos nombres coincidan con los de tu base de datos
+            body: JSON.stringify({ correo: email, contrasena: password, telefono: phone, nombre: name }) // Asegúrate de que estos nombres coincidan con los de tu base de datos
         });
 
         if (!response.ok) {
@@ -44,17 +39,18 @@ async function registrarUsuario( email, password) {
         alert('Ocurrió un error al registrar el usuario. Inténtalo de nuevo más tarde.');
     }
 }
+
 async function ConsultarDatosUsuario(email, password) {
     try {
       // Codificar la contraseña antes de enviarla (esto solo es para fines ilustrativos, NO es seguro)
       const encodedPassword = encodeURIComponent(password);
   
       // Realizar una solicitud GET al servidor con los parámetros
-      const response = await fetch(`http://192.168.0.101:8080/usuarios?correo=${encodeURIComponent(email)}&contrasena=${encodeURIComponent(password)}`, { //  IP Torre Pablo 
+      //const response = await fetch(`http://192.168.0.101:8080/usuarios?correo=${encodeURIComponent(email)}&contrasena=${encodeURIComponent(password)}`, { //  IP Torre Pablo 
       //const response = await fetch(`http://172.20.10.11:8080/usuarios?correo=${encodeURIComponent(email)}&contrasena=${encodeURIComponent(password)}`, { // IP Portatil Pablo Wifi Pablo
       //const response = await fetch(`http://192.168.0.20:8080/usuarios?correo=${encodeURIComponent(email)}&contrasena=${encodeURIComponent(password)}`, { // Ip ordenador vicente
       //const response = await fetch(`http://192.168.0.17:8080/usuarios?correo=${encodeURIComponent(email)}&contrasena=${encodeURIComponent(password)}`, { // Ip ordenador vicente
-      //const response = await fetch(`http://172.20.10.5:8080/usuarios?correo=${encodeURIComponent(email)}&contrasena=${encodeURIComponent(password)}`, { //  Ip Portátil visi Universidad
+      const response = await fetch(`http://192.168.0.25:8080/usuarios?correo=${encodeURIComponent(email)}&contrasena=${encodeURIComponent(password)}`, { //  Ip Portátil visi Universidad
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
@@ -101,15 +97,14 @@ document.getElementById('register-btn').addEventListener('click', function(event
     if (!emailRegex.test(email)) {
         alert('Por favor, introduce un email válido.'); // Mensaje de error
         return; // Salir de la función si el email no es válido
-	}
+    }
     // Validar formato de contraseña
-	const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{7,200}$/;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{7,200}$/;
     if (!passwordRegex.test(password)) {
         alert('La contraseña debe tener mínimo 8 caracteres, incluir al menos una mayúscula, una minúscula, un número y un carácter especial (!@#$%^&*)'); // Mensaje de error
         return; // Salir de la función si la contraseña no es válida
     }
-    registrarUsuario( email, password); // Llamar a la función para registrar el usuario3
-		
+    registrarUsuario(email, password, phone, name); // Llamar a la función para registrar el usuario
 });
 
 // Manejar el evento de clic en el botón de iniciar sesion
