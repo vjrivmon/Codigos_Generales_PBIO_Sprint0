055f26d5da764902dbaefd39f451874339fc5888
PabloMeana <pmeagon@epsg.upv.es>
2024-12-06 00:02:11 +0100

    Cambios Pablo |
    Se ven los nuevos datos de los gases clasificados en 3 graficas diferentes

--- Frontend/src/js/graficaGas.js
+++ Frontend/src/js/graficaGas.js
@@ -3,8 +3,13 @@
 // Requiere Chart.js y un backend que responda a la ruta '/mediciones/:idSensor' con los datos del sensor de gas en formato JSON
 const id_sensor = '00:1A:2B:3M:4D:5E'; // Ejemplo de ID de sensor
 const fechaSelector = document.getElementById('fecha-selector');
+const graficaSelector = document.getElementById('gas-selector');
 let mediciones = []; // Almacén local de todas las mediciones
 let grafica; // Variable global para almacenar la instancia de la gráfica
+const tipo = graficaSelector.value; // Tipo de gas seleccionado por defecto
+console.log('Tipo seleccionado:', tipo);
+const fecha = fechaSelector.value; // Fecha seleccionada por defecto
+console.log('Fecha seleccionada:', fecha);
 
 // Obtener todas las mediciones del sensor
 function cargarMediciones() {
@@ -15,92 +20,85 @@ function cargarMediciones() {
 
             // Guardar las mediciones en una variable global
             mediciones = data;
-
+             
             // Cargar datos iniciales para el 22/11/2024
-            filtrarMedicionesPorFecha('22/11/2024');
+            filtrarMediciones(fecha, tipo);
+            
+
         })
-        .catch(error => console.error('Error al obtener todas las mediciones:', error));
+        
 }
 
 // Filtrar mediciones por fecha seleccionada
-function filtrarMedicionesPorFecha(fecha) {
-    const datosFiltrados = mediciones.filter(item => item.fecha === fecha);
+function filtrarMediciones(fecha, tipo) {
+    const datosFiltrados = [];
+
+    for (const item of mediciones) {
+      if (item.fecha_hora.startsWith(fecha)) {
+        datosFiltrados.push(item);
+      }
+    }
+    
     console.log(`Datos filtrados para la fecha ${fecha}:`, datosFiltrados);
 
     if (datosFiltrados.length === 0) {
         console.log('No se encontraron datos para la fecha seleccionada.');
         return;
     }
+    const datosFiltradosO3 = [];
+    const datosFiltradosNO2 = [];
+    const datosFiltradosSO3 = [];
+    const datosFiltradosTemperatura = [];
+    
+    
 
-    // Extraer datos relevantes para la gráfica
-    const horas = datosFiltrados.map(item => item.hora.slice(0, 2));
-    const valoresO3 = datosFiltrados.map(item => item.valorO3);
-    const valoresNO2 = datosFiltrados.map(item => item.valorNO2);
-    const valoresSO3 = datosFiltrados.map(item => item.valorSO3);
+    for (const item of datosFiltrados) {
+      if (item.tipo_medicion === 'O3') {
+        datosFiltradosO3.push(item);
+      } else if (item.tipo_medicion === 'NO2') {
+        datosFiltradosNO2.push(item);
+      } else if (item.tipo_medicion === 'SO3') {
+        datosFiltradosSO3.push(item);
+      } else if (item.tipo_medicion === 'Temperatura') {
+        datosFiltradosTemperatura.push(item);
+      }
+    }
+    // Mostrar los datos filtrados en la consola
+    console.log(`Datos filtrados por O3 :`, datosFiltradosO3);
+    console.log(`Datos filtrados por NO2 :`, datosFiltradosNO2);
+    console.log(`Datos filtrados por SO3 :`, datosFiltradosSO3);
+    console.log(`Datos filtrados por Temperatura :`, datosFiltradosTemperatura);
+
+    const ultimosDatosFiltradosO3 = datosFiltradosO3.slice(-8);
+    const ultimosDatosFiltradosNO2 = datosFiltradosNO2.slice(-8);
+    const ultimosDatosFiltradosSO3 = datosFiltradosSO3.slice(-8);
+    const ultimosDatosFiltradosTemperatura = datosFiltradosTemperatura.slice(-8);
+
+    
+    // Mostrar los datos filtrados en la consola de las ultimas 8 horas
+    console.log(`Datos filtrados por O3 de las ultimas 8 horas:`, ultimosDatosFiltradosO3);
+    console.log(`Datos filtrados por NO2 de las ultimas 8 horas:`, ultimosDatosFiltradosNO2);
+    console.log(`Datos filtrados por SO3 de las ultimas 8 horas:`, ultimosDatosFiltradosSO3);
+    console.log(`Datos filtrados por Temperatura de las ultimas 8 horas:`, ultimosDatosFiltradosTemperatura);
+
+    // Extraer los caracteres desde el índice 5 al 7 de la propiedad fecha_hora para la gráfica
+    const horas = ultimosDatosFiltradosO3.map(item => item.fecha_hora.slice(11, 13));
+    console.log('Horas para la gráfica:', horas);
+
+    const valoresO3 = datosFiltradosO3.map(item => item.valor);
+    const valoresNO2 = datosFiltradosNO2.map(item => item.valor);
+    const valoresSO3 = datosFiltradosSO3.map(item => item.valor);
+    const valoresTemperatura = datosFiltradosTemperatura.map(item => item.valor);
 
     // Actualizar los valores medios
-    document.getElementById('media-O3').textContent = `Valor Medio O3: ${calcularMediaPonderada(valoresO3).toFixed(2)} ppm`;
-    document.getElementById('media-NO2').textContent = `Valor Medio NO2: ${calcularMediaPonderada(valoresNO2).toFixed(2)} ppm`;
-    document.getElementById('media-SO3').textContent = `Valor Medio SO3: ${calcularMediaPonderada(valoresSO3).toFixed(2)} ppm`;
-
-    // Actualizar la gráfica
-    actualizarGrafica(horas, valoresO3, valoresNO2, valoresSO3);
+        document.getElementById('media-O3').innerHTML = `<i class="fas fa-cloud"></i>   O3: ${calcularMediaPonderada(valoresO3).toFixed(2)} ppm`;
+    document.getElementById('media-NO2').innerHTML = `<i class="fas fa-cloud"></i>   NO2: ${calcularMediaPonderada(valoresNO2).toFixed(2)} ppm`;
+    document.getElementById('media-SO3').innerHTML = `<i class="fas fa-cloud"></i>   SO3: ${calcularMediaPonderada(valoresSO3).toFixed(2)} ppm`;
+    document.getElementById('media-Temperatura').innerHTML = `<i class="fas fa-cloud"></i>   Temperatura: ${calcularMediaPonderada(valoresTemperatura).toFixed(2)} ºC`;
+    actualizarGraficaPorTipo(tipo, horas, valoresO3, valoresNO2, valoresSO3, valoresTemperatura);
 }
 
-// Actualiza la gráfica con nuevos datos
-function actualizarGrafica(horas, valoresO3, valoresNO2, valoresSO3) {
-    const ctx = document.getElementById('graficaGas').getContext('2d');
 
-    // Destruir la gráfica anterior si ya existe
-    if (grafica) {
-        grafica.destroy();
-    }
-
-    // Crear una nueva gráfica
-    grafica = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: horas,
-            datasets: [
-                {
-                    label: 'Valor de O3 (ppm)',
-                    data: valoresO3,
-                    borderColor: 'rgba(59, 59, 59, 1)',
-                    backgroundColor: 'rgba(59, 59, 59, 0.2)',
-                    fill: false,
-                    tension: 0.4,
-                },
-                {
-                    label: 'Valor de NO2 (ppm)',
-                    data: valoresNO2,
-                    borderColor: 'rgba(57, 88, 134, 1)',
-                    backgroundColor: 'rgba(57, 88, 134, 0.2)',
-                    fill: false,
-                    tension: 0.4,
-                },
-                {
-                    label: 'Valor de SO3 (ppm)',
-                    data: valoresSO3,
-                    borderColor: 'rgba(177, 201, 239, 1)',
-                    backgroundColor: 'rgba(177, 201, 239, 0.2)',
-                    fill: false,
-                    tension: 0.4,
-                },
-            ],
-        },
-        options: {
-            responsive: true,
-            scales: {
-                x: {
-                    type: 'category',
-                },
-                y: {
-                    beginAtZero: true,
-                },
-            },
-        },
-    });
-}
 
 // Calcular la media ponderada
 function calcularMediaPonderada(valores) {
@@ -108,11 +106,299 @@ function calcularMediaPonderada(valores) {
     return sumaValores / valores.length;
 }
 
+
 // Manejar el cambio de fecha en el selector
 fechaSelector.addEventListener('change', () => {
-    const fechaSeleccionada = fechaSelector.value;
-    filtrarMedicionesPorFecha(fechaSeleccionada);
+    const fecha2 = fechaSelector.value;
+    console.log('Fecha seleccionada:', fecha);
+    filtrarMediciones(fecha2,tipo);
 });
+
+// Manejar el cambio de tipo de gas en el selector
+graficaSelector.addEventListener('change', () => {
+    const tipo2 = graficaSelector.value;
+    console.log('Tipo seleccionado:', tipo);
+    filtrarMediciones(fecha, tipo2);
+});
+
+// Actualizar la gráfica según el tipo seleccionado
+function actualizarGraficaPorTipo(tipo, horas, valoresO3, valoresNO2, valoresSO3, valoresTemperatura) {
+    const ctx = document.getElementById('graficaGas').getContext('2d');
+    // Destruir la gráfica anterior si ya existe
+    if (grafica) {
+        grafica.destroy();
+    }
+    var gradient = ctx.createLinearGradient(0, 0, 0, 400);
+    gradient.addColorStop(0, 'green'); // Color en el valor mínimo (0)
+    gradient.addColorStop(1, 'red');   // Color en el valor máximo
+    
+    switch (tipo) {
+        case 'O3':
+            // Crear una nueva gráfica
+            grafica = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: horas,
+                datasets: [
+                    {
+                        label: 'Valor de O3 (ppm)',
+                        data: valoresO3,
+                        borderColor: 'rgba(59, 59, 59, 1)',
+                        backgroundColor: 'rgba(59, 59, 59, 0.2)',                        
+                        fill: false,
+                        tension: 0.4,
+                    },
+                ],
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    x: {
+                        type: 'category',
+                    },
+                    y: {
+                        beginAtZero: true,
+                        max: 250, // Aquí defines el valor máximo del eje Y
+                    },
+                },
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Gráfica con los datos recopilados por los sensores en las últimas 8 horas',
+                        color: '#395886',
+                        font: {
+                            size: 40,
+                        },
+                        margin:{
+                            bottom: 16
+                        },
+                        align: 'center' 
+
+                    },
+                    
+                }
+                
+            },
+            });
+            break;
+        case 'NO2':
+            // Crear una nueva gráfica
+            grafica = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: horas,
+                    datasets: [
+                        {
+                            label: 'Valor de NO2 (ppm)',
+                            data: valoresNO2,
+                            borderColor: 'rgba(59, 59, 59, 1)',
+                            backgroundColor: 'rgba(59, 59, 59, 0.2)',                            
+                            fill: false,
+                            tension: 0.4,
+                        },
+                    ],
+                },
+                options: {
+                    responsive: true,
+                    scales: {
+                        x: {
+                            type: 'category',
+                        },
+                        y: {
+                            beginAtZero: true,
+                            max: 200, // Aquí defines el valor máximo del eje Y
+                        },
+                    },
+                    plugins: {
+                        title: {
+                            display: true,
+                            text: 'Gráfica con los datos recopilados por los sensores en las últimas 8 horas',
+                            color: '#395886',
+                            font: {
+                                size: 40,
+                            },
+                            margin:{
+                                bottom: 16
+                            },
+                            align: 'center' 
+
+                        },
+                        
+                    }
+           
+                },
+                });
+            break;
+        case 'SO3':
+            // Crear una nueva gráfica
+            grafica = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: horas,
+                    datasets: [
+                        {
+                            label: 'Valor de SO3 (ppm)',
+                            data: valoresSO3,
+                            borderColor: 'rgba(59, 59, 59, 1)',
+                            backgroundColor: 'rgba(59, 59, 59, 0.2)',                            
+                            fill: false,
+                            tension: 0.4,
+                        },
+                    ],
+                },
+                options: {
+                    responsive: true,
+                    scales: {
+                        x: {
+                            type: 'category',
+                        },
+                        y: {
+                            beginAtZero: true,
+                            max: 350, // Aquí defines el valor máximo del eje Y
+                        },
+                    },
+                    plugins: {
+                        title: {
+                            display: true,
+                            text: 'Gráfica con los datos recopilados por los sensores en las últimas 8 horas',
+                            color: '#395886',
+                            font: {
+                                size: 40,
+                            },
+                            margin:{
+                                bottom: 16
+                            },
+                            align: 'center' 
+
+                        },
+                        
+                    }
+               
+                },
+                });
+            break;
+        case 'Temperatura':
+            // Crear una nueva gráfica
+            grafica = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: horas,
+                    datasets: [
+                        {
+                            label: 'Valor de Temperatura (ºC)',
+                            data: valoresTemperatura,
+                            borderColor: 'rgba(59, 59, 59, 1)',
+                            backgroundColor: 'rgba(59, 59, 59, 0.2)',
+                            fill: false,
+                            tension: 0.4,
+                        },
+                    ],
+                },
+                options: {
+                    responsive: true,
+                    scales: {
+                        x: {
+                            type: 'category',
+                        },
+                        y: {
+                            min: 20, // Aquí defines el valor mínimo del eje Y
+                            max: 40 // Aquí defines el valor máximo del eje Y 
+                        },
+                    },
+                    plugins: {
+                        title: {
+                            display: true,
+                            text: 'Gráfica con los datos recopilados por los sensores en las últimas 8 horas',
+                            color: '#395886',
+                            font: {
+                                size: 40,
+                            },
+                            margin:{
+                                bottom: 16
+                            },
+                            align: 'center' 
+
+                        },
+                        
+                    }
+                },
+            });
+            break;
+        case 'Todos':
+            grafica = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: horas,
+                    datasets: [
+                        {
+                            label: 'Valor de O3 (ppm)',
+                            data: valoresO3,
+                            borderColor: 'rgba(59, 59, 59, 1)',
+                            backgroundColor: 'rgba(59, 59, 59, 0.2)',
+                            fill: false,
+                            tension: 0.4,
+                        },
+                        {
+                            label: 'Valor de NO2 (ppm)',
+                            data: valoresNO2,
+                            borderColor: 'rgba(57, 88, 134, 1)',
+                            backgroundColor: 'rgba(57, 88, 134, 0.2)',
+                            fill: false,
+                            tension: 0.4,
+                        },
+                        {
+                            label: 'Valor de SO3 (ppm)',
+                            data: valoresSO3,
+                            borderColor: 'rgba(177, 201, 239, 1)',
+                            backgroundColor: 'rgba(177, 201, 239, 0.2)',
+                            fill: false,
+                            tension: 0.4,
+                        },
+                        {
+                            label: 'Valor de Temperatura (ºC)',
+                            data: valoresTemperatura,
+                            borderColor: 'rgba(178, 253, 178, 1)',
+                            backgroundColor: 'rgba(178, 253, 178, 0.2)',
+                            fill: false,
+                            tension: 0.4,
+                        },
+                    ],
+                },
+                options: {
+                    responsive: true,
+                    scales: {
+                        x: {
+                            type: 'category',
+                        },
+                        y: {
+                            beginAtZero: true,
+                        },
+                    },
+                    plugins: {
+                        title: {
+                            display: true,
+                            text: 'Gráfica con los datos recopilados por los sensores en las últimas 8 horas',
+                            color: '#395886',
+                            font: {
+                                size: 40,
+                            },
+                            margin:{
+                                bottom: 16
+                            },
+                            align: 'center' 
+
+                        }
+                        
+                    }
+                },
+            });
+            return;
+    }
+
+    
+}
+
+
 
 // Cargar todas las mediciones al iniciar
 cargarMediciones();
