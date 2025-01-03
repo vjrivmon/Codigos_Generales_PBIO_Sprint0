8fcb43d39cb66d295c8a0c52548a45b55773685e
Mimi Vladeva <mvladev@epsg.upv.es>
2025-01-03 18:34:12 +0100

    Cambios mimi |
    Android mapa viernes sprint

--- Android/Biometria3a/app/src/main/java/com/example/biometria3a/MenuHandler.java
+++ Android/Biometria3a/app/src/main/java/com/example/biometria3a/MenuHandler.java
@@ -66,6 +66,11 @@ public class MenuHandler {
             // If the context is an instance of AppCompatActivity (or any Activity)
             context.startActivity(new Intent(context, QrActivity.class));
         }
+        else if (item.getItemId() == R.id.map) {
+            Toast.makeText(context, "mapa", Toast.LENGTH_SHORT).show();
+            // If the context is an instance of AppCompatActivity (or any Activity)
+            context.startActivity(new Intent(context,webmapAc.class));
+        }
     }
 
 }
